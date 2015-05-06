99626ea24b9ed0fdf0a85a4400c2b640bafe7c2b
charlychiu <charlychiu@gmail.com>
2015-05-07 00:34:04 +0800

    testing

---
+++ dynamic aeray teating/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  dynamic aeray teating
+//
+//  Created by charlychiu on 2015/5/4.
+//  Copyright (c) 2015年 charlychiu. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int **array;
+    array =new int *[5];
+    
+    for(int i=0;i<5;i++)
+        array[i]=new int[5];
+    
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            array[i][j]=i*j;
+            cout<<setw(5)<<array[i][j];
+        }
+        cout<<endl;
+    }
+    
+    
+    
+    return 0;
+}
